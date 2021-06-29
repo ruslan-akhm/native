10e912276e82005f627de5de60bd52bfd0f2a6c4
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-06-28 21:54:38 -0400

    added handlers

--- App.js
+++ App.js
@@ -1,31 +1,67 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    //console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.container}>
         <TextInput
           placeholder="new goal"
-          style={{
-            width: "80%",
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-          }}
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="Add" />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <ScrollView>
+        {courseGoals.map(goal => (
+          <View key={goal} style={styles.item}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  item: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+  },
+});
